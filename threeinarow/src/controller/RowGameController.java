dbe18f0902fbb59886221d87a827200556489a57
Pradhiksha27 <pradhiksha27may@gmail.com>
2020-10-25 14:25:02 +0530

    Fix: Best practices violation

--- threeinarow/src/controller/RowGameController.java
+++ threeinarow/src/controller/RowGameController.java
@@ -22,17 +22,30 @@ public class RowGameController {
     public static final String GAME_END_NOWINNER = "Game ends in a draw";
 
     public RowGameModel gameModel;
-    public RowGameGUI gameView;
+    public RowGameRulesStrategy gameStrategy;
 
+    //Addresses issue #1(Encapsulation)
+    private int numRows;
+    private int numCols;
 
     /**
      * Creates a new game initializing the GUI.
      */
-    public RowGameController() {
-		gameModel = new RowGameModel();
-		gameView = new RowGameGUI(this);
+    public RowGameController(int rows, int cols, int gameRule) {
+		gameModel = new RowGameModel(rows, cols);
+		numRows = rows;
+		numCols = cols;
+
+		if(gameRule == 1)
+		{
+			gameStrategy = new TIRrules();
+		}
+		else
+		{
+			gameStrategy = new TicTacToe();
+		}
 	
-		resetGame();
+		gameStrategy.resetGame(gameModel, rows, cols);
     }
 
     public RowGameModel getModel() {
@@ -52,39 +65,126 @@ public class RowGameController {
      *
      * @param block The block to be moved to by the current player
      */
-    public void move(JButton block) {
-	gameModel.movesLeft = gameModel.movesLeft - 1;
+    //Addresses issue #3 (Testability)
+    public void move(int row, int col) {
+    	if(row < numRows && row >=0 && col < numCols && col >= 0 && gameModel.blocksData[row][col].getIsLegalMove() == false)
+    	{
+    		return;
+    	}
 
-	gameView.update(gameModel);
+		gameModel.movesLeft = gameModel.movesLeft - 1;
+		String player = gameModel.player;
+		int movesLeft = gameModel.movesLeft;
+
+		int rowIndex = row;
+		int colIndex = col;
+		//Check if player 1 is the winner
+		if (player.equals("1")) 
+		{
+			gameModel.setBlocksValue(rowIndex, colIndex, "X");
+			gameStrategy.legalMoves(gameModel, rowIndex, colIndex);
+			gameModel.setPlayer("2");
+
+			if (Winner(rowIndex, colIndex) == 1) 
+			{
+				gameModel.setFinalResult("Player 1 is the winner!");
+				endGame();
+			} 
+			else if (movesLeft == 0) 
+			{
+				gameModel.setFinalResult(GAME_END_NOWINNER);
+			}
+		} 
+		//Check if player 2 is the winner
+		else 
+		{
+			gameModel.setBlocksValue(rowIndex, colIndex, "O");
+			gameStrategy.legalMoves(gameModel, rowIndex, colIndex);
+			gameModel.setPlayer("1");
+
+			if (Winner(rowIndex, colIndex) == 2) 
+			{
+				gameModel.setFinalResult("Player 2 is the winner!");
+				endGame();
+			} 
+			else if (movesLeft == 0) 
+			{
+				gameModel.setFinalResult(GAME_END_NOWINNER);
+			}
+		}
+		gameView.update(gameModel);
     }
 
     /**
      * Ends the game disallowing further player turns.
      */
+    //Addresses issue #2 (Extensibility)
     public void endGame() {
-		for(int row = 0;row<3;row++) {
-	    	for(int column = 0;column<3;column++) {
-				this.gameModel.blocksData[row][column].setIsLegalMove(false);
+		for(int row = 0;row<numRows;row++) {
+	    	for(int column = 0;column<numCols;column++) {
+				this.gameModel.setIsLegal(row, column, false);
 	    	}
 		}
-
-		gameView.update(gameModel);
     }
 
-    /**
-     * Resets the game to be able to start playing again.
-     */
-    public void resetGame() {
-        for(int row = 0;row<3;row++) {
-            for(int column = 0;column<3;column++) {
-                gameModel.blocksData[row][column].reset();
-		// Enable the bottom row
-	        gameModel.blocksData[row][column].setIsLegalMove(row == 2);
-            }
-        }
-		gameModel.player = "1";
-		gameModel.movesLeft = 9;
-		gameModel.setFinalResult(null);
-		gameView.update(gameModel);
+    //Takes as input the row & column indices and returns the value of the corresponding blocksData if valid
+    public String getContentIfValid(int rowIndex, int colIndex){
+    	if(rowIndex < numRows && rowIndex >=0 && colIndex < numCols && colIndex >= 0){
+    		return gameModel.blocksData[rowIndex][colIndex].getContents();
+    	}
+    	return "Invalid indices!"
     }
+
+    //Return the 1,2,0 corresponding to whichever player has won the game
+    public int Winner(int rowIndex, int colIndex) 
+    {	
+    	//Checking horizontally
+		for (int i = 0; i < numCols - 2; i++) 
+		{
+			String stringCheck = (gameModel.blocksData[rowIndex][i].getContents() + gameModel.blocksData[rowIndex][i + 1].getContents() + gameModel.blocksData[rowIndex][i + 2].getContents());
+			
+			if (stringCheck.equals("XXX")) 
+			{
+				return 1;
+			} 
+			else if (stringCheck.equals("OOO")) 
+			{
+				return 2;
+			}
+		}
+		//Checking vertically
+		for (int j = 0; j < numRows - 2; j++) 
+		{
+			String stringCheck = (gameModel.blocksData[i][colIndex].getContents() + gameModel.blocksData[i + 1][colIndex].getContents() + gameModel.blocksData[i + 2][colIndex].getContents());
+			
+			if (stringCheck.equals("XXX")) 
+			{
+				return 1;
+			} 
+			else if (stringCheck.equals("OOO")) 
+			{
+				return 2;
+			}
+		}
+		
+		//Checking diagonally
+		String d1 = getContentIfValid(rowIndex, colIndex) + getContentIfValid(rowIndex - 1, colIndex + 1) + getContentIfValid(rowIndex - 2, colIndex + 2);
+		String d2 = getContentIfValid(rowIndex, colIndex) + getContentIfValid(rowIndex - 1, colIndex - 1) + getContentIfValid(rowIndex - 2, colIndex - 2);
+		String d3 = getContentIfValid(rowIndex, colIndex) + getContentIfValid(rowIndex + 1, colIndex - 1) + getContentIfValid(rowIndex + 2, colIndex - 2);
+		String d4 = getContentIfValid(rowIndex, colIndex) + getContentIfValid(rowIndex + 1, colIndex + 1) + getContentIfValid(rowIndex + 2, colIndex + 2);
+
+		String d1d3 = getContentIfValid(rowIndex, colIndex) + getContentIfValid(rowIndex + 1, colIndex + 1) + getContentIfValid(rowIndex - 1, colIndex - 1);
+		String d2d4 = getContentIfValid(rowIndex, colIndex) + getContentIfValid(rowIndex + 1, colIndex - 1) + getContentIfValid(rowIndex - 1, colIndex + 1);
+
+		if (d1.equals("XXX") || d2.equals("XXX") || d3.equals("XXX") || q4Diagonal.equals("XXX") || d1d3.equals("XXX") || d2d4.equals("XXX")) 
+		{
+			return 1;
+		} 
+
+		else if (d1.equals("OOO") || d2.equals("OOO") || d3.equals("OOO") || d4.equals("OOO") || d1d3.equals("OOO") || d2d4.equals("OOO")) 
+		{
+			return 2;
+		}
+		return 0;
+	}
 }
--- threeinarow/src/model/RowGameModel.java
+++ threeinarow/src/model/RowGameModel.java
@@ -1,36 +1,73 @@
 package model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class RowGameModel 
 {
     public static final String GAME_END_NOWINNER = "Game ends in a draw";
 
-    public RowBlockModel[][] blocksData = new RowBlockModel[3][3];
+    public RowBlockModel[][] blocksData;
 
     /**
      * The current player taking their turn
      */
     public String player = "1";
     public int movesLeft = 9;
-
     private String finalResult = null;
 
+    private final PropertyChangeSupport change = new PropertyChangeSupport(this);
 
-    public RowGameModel() {
-	super();
+    //Addresses issue #2 (Extensibility)
+    public RowGameModel(int numRows, int numCols) 
+    {
+	    super();
+        blocksData = new RowBlockModel[numRows][numCols];
+        movesLeft = numRows * numCols;
 
-	for (int row = 0; row < 3; row++) {
-	    for (int col = 0; col < 3; col++) {
-		blocksData[row][col] = new RowBlockModel(this);
-	    } // end for col
-	} // end for row
+
+	    for (int row = 0; row < numRows; row++) {
+	        for (int col = 0; col < numCols; col++) {
+	           blocksData[row][col] = new RowBlockModel(this);
+	        } // end for col
+	    } // end for row
     }
 
     public String getFinalResult() {
-	return this.finalResult;
+	   return this.finalResult;
     }
 
+    //Assigns the value of this.finalResult to the "finalResult" parameter and fires an event.
     public void setFinalResult(String finalResult) {
-	this.finalResult = finalResult;
+        String oldResult = this.finalResult;
+	    this.finalResult = finalResult;
+        this.change.firePropertyChange("finalResult", oldResult, this.finalResult)
+    }
+
+    //Assigns this.player to the "player" parameter and fires an event.
+      public void setPlayer(String player) {
+        String oldPlayer = this.player;
+        this.player = player;
+        this.change.firePropertyChange("player", oldPlayer, this.player);
+    }
+
+    //Assigns the corresponding blocksdata value to the "value" parameter and fires an event.
+    public void setBlocksValue(int rowIndex, int colIndex, String value) {
+        this.blocksData[rowIndex][colIndex].setContents(value);
+        this.change.firePropertyChange("value", "", value);
+    }
+
+    //Assigns the correspding blocksdata value to the "isLegal" parameter and fires an event.
+    public void setIsLegal(int rowIndex, int colIndex, boolean isLegal){
+        boolean oldValue = this.blocksData[rowIndex][colIndex].getIsLegalMove();
+        this.blocksData[rowIndex][colIndex].setIsLegalMove(isLegal);
+        this.change.firePropertyChange("isLegal", oldValue, isLegal);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.change.addPropertyChangeListener(listener);
+    }
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.change.removePropertyChangeListener(listener);
     }
 }
