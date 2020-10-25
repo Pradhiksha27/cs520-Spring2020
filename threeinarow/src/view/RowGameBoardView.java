e2712745cb78238c30d2eb66873f6112437dfdef
Pradhiksha27 <pradhiksha27may@gmail.com>
2020-10-25 16:40:32 +0530

    fix: indentation, readibility & encapsulation

--- threeinarow/src/view/RowGameBoardView.java
+++ threeinarow/src/view/RowGameBoardView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -17,25 +19,50 @@ import model.RowGameModel;
 
 public class RowGameBoardView implements RowGameView
 {
-    public JButton[][] blocks = new JButton[3][3];
+    public JButton[][] blocks;
     public JPanel gamePanel = new JPanel(new FlowLayout());
 
-    
-    public RowGameBoardView(RowGameController gameController) {
-	super();
+    //Addresses issue #1(Encapsulation)
+    private int numRows;
+    private int numCols;
 
-        JPanel game = new JPanel(new GridLayout(3,3));
+    public RowGameBoardView(RowGameController gameController, int rows, int cols) 
+    {
+        super();
+
+        JPanel game = new JPanel(new GridLayout(rows,cols));
         gamePanel.add(game, BorderLayout.CENTER);
 	
-       // Initialize a JButton for each cell of the 3x3 game board.
-        for(int row = 0; row<3; row++) {
-            for(int column = 0; column<3 ;column++) {
+        numRows = rows;
+        numCols = cols;
+        blocks = new JButton[rows][cols];
+
+        gameController.gameModel.addPropertyChangeListener(new PropertyChangeListener() 
+        {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) 
+            {
+                update(gameController.gameModel);
+            }
+        });
+
+        // Initialize a JButton for each cell of the rows x cols game board.
+        for(int row = 0; row<rows; row++) 
+        {
+            for(int column = 0; column<cols; column++) 
+            {
+                int chosenRow = row;
+                int chosenCol = column;
+
                 blocks[row][column] = new JButton();
                 blocks[row][column].setPreferredSize(new Dimension(75,75));
                 game.add(blocks[row][column]);
-                blocks[row][column].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-			gameController.move((JButton)e.getSource());
+                
+                blocks[row][column].addActionListener(new ActionListener() 
+                {
+                    public void actionPerformed(ActionEvent e) 
+                    {
+                        gameController.move(chosenRow, chosenCol);
                     }
                 });
             }
@@ -48,12 +75,15 @@ public class RowGameBoardView implements RowGameView
      *
      * @param gameModel The current game model
      */
-    public void update(RowGameModel gameModel) {
-	for (int row = 0; row < 3; row++) {
-	    for (int column = 0; column < 3; column++) {
-		this.updateBlock(gameModel, row, column);
-	    } // end for col
-	} // end for row	
+    public void update(RowGameModel gameModel) 
+    {
+        for (int row = 0; row < numRows; row++) 
+        {
+	       for (int column = 0; column < numCols; column++) 
+           {
+		      this.updateBlock(gameModel, row, column);
+	       } // end for col
+	   } // end for row	
     }
 
     /**
@@ -64,8 +94,9 @@ public class RowGameBoardView implements RowGameView
      * @param row The row that contains the block
      * @param column The column that contains the block
      */
-    protected void updateBlock(RowGameModel gameModel, int row, int col) {
-	blocks[row][col].setText(gameModel.blocksData[row][col].getContents());
-	blocks[row][col].setEnabled(gameModel.blocksData[row][col].getIsLegalMove());	
+    protected void updateBlock(RowGameModel gameModel, int row, int col) 
+    {
+        blocks[row][col].setText(gameModel.blocksData[row][col].getContents());
+        blocks[row][col].setEnabled(gameModel.blocksData[row][col].getIsLegalMove());	
     }
 }
--- threeinarow/src/view/RowGameGUI.java
+++ threeinarow/src/view/RowGameGUI.java
@@ -23,29 +23,33 @@ public class RowGameGUI implements RowGameView
     /**
      * Creates a new game initializing the GUI.
      */
-    public RowGameGUI(RowGameController gameController) {
-	this.gameController = gameController;
+    public RowGameGUI(RowGameController gameController, int rows, int cols) 
+    {
+        this.gameController = gameController;
 	
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.setSize(new Dimension(500, 350));
+        gui.setSize(new Dimension(1000, 750));
         gui.setResizable(true);
 
-	gameBoardView = new RowGameBoardView(this.gameController);
+        gameBoardView = new RowGameBoardView(this.gameController, rows, cols);
         JPanel gamePanel = gameBoardView.gamePanel;
 
         JPanel options = new JPanel(new FlowLayout());
         options.add(reset);
 
-	gameStatusView = new RowGameStatusView(this.gameController);
+        gameStatusView = new RowGameStatusView(this.gameController);
         JPanel messages = gameStatusView.messages;
 
         gui.add(gamePanel, BorderLayout.NORTH);
         gui.add(options, BorderLayout.CENTER);
         gui.add(messages, BorderLayout.SOUTH);
 
-        reset.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                gameController.resetGame();
+        gui.setVisible(true);
+        reset.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                gameController.gameStrategy.resetGame(gameController.gameModel, rows, cols);
             }
         });
     }
@@ -56,9 +60,9 @@ public class RowGameGUI implements RowGameView
      *
      * @param gameModel The current game model
      */
-    public void update(RowGameModel gameModel) {
-	gameBoardView.update(gameModel);
-
-	gameStatusView.update(gameModel);
+    public void update(RowGameModel gameModel) 
+    {
+	   gameBoardView.update(gameModel);
+	   gameStatusView.update(gameModel);
     }
 }
